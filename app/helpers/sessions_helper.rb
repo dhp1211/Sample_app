9d66ae0025d0989bc8d9b925a7249989541ae75c
dhp1211 <dhp1211@gmail.com>
2015-10-01 14:42:01 +0700

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,10 +3,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   def remember(user)
-
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+  def current_user?(user)
+    user == current_user
   end
   def current_user
     if (user_id = session[:user_id])
@@ -31,5 +33,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  # Redirect to store location (or to the default)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
