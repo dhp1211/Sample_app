46cf2a922680befa23d0be2a9c32a071b7d2a59d
dhp1211 <dhp1211@gmail.com>
2015-10-07 08:38:29 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,14 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
+    
     if @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'example.com'
+  host = "localhost:3000"
   config.action_mailer.default_url_options = { host: host }
 
   # Raises error for missing translations
